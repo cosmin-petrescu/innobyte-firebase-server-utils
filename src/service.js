685eb4e7beef2769218754751d5f650474b3cc29
Cosmin Petrescu <cosmin.petrescu@innobyte.com>
2016-07-22 08:50:06 +0300

    Initial commit

---
+++ src/service.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const q = require('q'),
+    firebase = require('firebase');
+
+const Service = {
+
+    /**
+     * Verify that uid and token match. Use as middleware on routes that require admin access.
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    verifyAuth: function (req, res, next) {
+        const deferred = q.defer(),
+            token = req.header('x-auth-token'),
+            uid = req.header('x-auth-uid'),
+            auth = firebase.auth();
+
+        auth.verifyIdToken(token)
+            .then(function (decodedToken) {
+                if (uid !== decodedToken.sub) {
+                    return deferred.reject({
+                        message: 'Unauthorized request',
+                        code: 'INVALID_REQUEST'
+                    });
+                }
+
+                deferred.resolve(true);
+            })
+            .catch(function () {
+                deferred.reject({
+                    message: 'Unauthorized request',
+                    code: 'INVALID_REQUEST'
+                });
+            });
+
+        deferred.promise
+            .then(function () {
+                next();
+            })
+            .catch(function (error) {
+                res.end(JSON.stringify(error));
+            });
+    },
+
+    /**
+     * Verifies that user is admin. Use as middleware on routes that require admin access.
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    verifyAdmin: function (req, res, next) {
+        const deferred = q.defer(),
+            uid = req.header('x-auth-uid'),
+            db = firebase.database().ref();
+
+        db.child('users/' + uid)
+            .once('value', function (snapshot) {
+                if (snapshot.exists()) {
+                    if (!snapshot.val().admin) {
+                        deferred.reject(false);
+                    } else {
+                        deferred.resolve(true);
+                    }
+                } else {
+                    deferred.reject(false);
+                }
+            });
+
+        deferred.promise
+            .then(function () {
+                next();
+            })
+            .catch(function () {
+                res.end(JSON.stringify({message: 'Unauthorized request', code: 'INVALID_REQUEST'}));
+            });
+    }
+};
+
+module.exports = Service;
